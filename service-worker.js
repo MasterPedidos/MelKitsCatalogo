ddc00866644d920d8e5687a1aea27d79515e9d7a
MasterPedidos <mjunior.ribeiro@terra.com.br>
2025-12-02 09:00:26 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,42 @@
+const CACHE_NAME = "melkits-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./painel.html",
+  "./manifest.json",
+  "./produtos.json",
+  "./icone/icon_192.png"
+];
+
+// Instalação do SW
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Ativação
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
 
